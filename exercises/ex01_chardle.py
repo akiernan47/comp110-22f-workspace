f9f66912ef0aa432fddabf981c9bc48717380ddd
Aidan Kiernan <akiernan@unc.edu>
2022-08-20 21:34:16 -0400

    Finalized Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -3,41 +3,41 @@
 __author__ = "730515426"
 
 user_word: str = input("Enter a 5-character word: ")
-if len(user_word) != 5 :
+if len(user_word) != 5:
     print("Error: Word must contain 5 characters")
     exit()
 
-search_chr: chr = input("Enter a single character: ")
-if len(search_chr) != 1 :
+search_letter: str = input("Enter a single character: ")
+if len(search_letter) != 1:
     print("Error: Character must be a single character")
     exit()
-print("Searching for " + search_chr + " in " + user_word)
+print("Searching for " + search_letter + " in " + user_word)
 
 chr_match_count: int = 0
-if search_chr == user_word[0]:
+if search_letter == user_word[0]:
     chr_match_count += 1
-    print(search_chr + " found at index 0")
+    print(search_letter + " found at index 0")
 
-if search_chr == user_word[1]:
+if search_letter == user_word[1]:
     chr_match_count += 1
-    print(search_chr + " found at index 1")
+    print(search_letter + " found at index 1")
 
-if search_chr == user_word[2]:
+if search_letter == user_word[2]:
     chr_match_count += 1
-    print(search_chr + " found at index 2")
+    print(search_letter + " found at index 2")
 
-if search_chr == user_word[3]:
+if search_letter == user_word[3]:
     chr_match_count += 1
-    print(search_chr + " found at index 3")
+    print(search_letter + " found at index 3")
 
-if search_chr == user_word[4]:
+if search_letter == user_word[4]:
     chr_match_count += 1
-    print(search_chr + " found at index 4")
+    print(search_letter + " found at index 4")
 
 if chr_match_count > 0:
     if chr_match_count == 1:
-        print("1 instance of " + search_chr + " found in " + user_word)
+        print("1 instance of " + search_letter + " found in " + user_word)
     else:
-        print(str(chr_match_count) + " instances of " + search_chr + " found in " + user_word)
+        print(str(chr_match_count) + " instances of " + search_letter + " found in " + user_word)
 else:
-    print("No instances of " + search_chr + " found in " + user_word)
+    print("No instances of " + search_letter + " found in " + user_word)
