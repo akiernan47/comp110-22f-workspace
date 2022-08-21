1b8086fcd205fffd7d33e0a5fd8c59f8f121072d
Aidan Kiernan <akiernan@unc.edu>
2022-08-20 21:25:16 -0400

    Finished Exercise 1

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,43 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730515426"
+
+user_word: str = input("Enter a 5-character word: ")
+if len(user_word) != 5 :
+    print("Error: Word must contain 5 characters")
+    exit()
+
+search_chr: chr = input("Enter a single character: ")
+if len(search_chr) != 1 :
+    print("Error: Character must be a single character")
+    exit()
+print("Searching for " + search_chr + " in " + user_word)
+
+chr_match_count: int = 0
+if search_chr == user_word[0]:
+    chr_match_count += 1
+    print(search_chr + " found at index 0")
+
+if search_chr == user_word[1]:
+    chr_match_count += 1
+    print(search_chr + " found at index 1")
+
+if search_chr == user_word[2]:
+    chr_match_count += 1
+    print(search_chr + " found at index 2")
+
+if search_chr == user_word[3]:
+    chr_match_count += 1
+    print(search_chr + " found at index 3")
+
+if search_chr == user_word[4]:
+    chr_match_count += 1
+    print(search_chr + " found at index 4")
+
+if chr_match_count > 0:
+    if chr_match_count == 1:
+        print("1 instance of " + search_chr + " found in " + user_word)
+    else:
+        print(str(chr_match_count) + " instances of " + search_chr + " found in " + user_word)
+else:
+    print("No instances of " + search_chr + " found in " + user_word)
