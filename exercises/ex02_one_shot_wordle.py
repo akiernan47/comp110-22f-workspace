fbb4e1b0f8aa9cca19b7a24b54fcae4dffdec579
Aidan Kiernan <akiernan@unc.edu>
2022-08-31 01:50:23 -0400

    woohoo

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -7,7 +7,7 @@ secret_len: int = len(secret_word)
 
 # Prompting user to input their guess of the secret word as a variable of the string type
 user_word: str = input(f"What is your {secret_len}-letter guess? ")
-# Executes while the guess does not match the length of the secret word for error checking
+# Executes while the length of the words do not match for error checking
 while len(user_word) != secret_len:
     user_word = input(f"That was not {secret_len} letters! Try again: ")
 
@@ -23,17 +23,17 @@ emoji_str: str = ""
 
 # Executes while the index value is less than the length of the secret word:
 while idx < secret_len:
-    # Checks for match of a character at a given index for the guess and secret
-    # If match present:
+    # Checks for matching character at a given index for the guess and secret
+    # If match at index:
     if user_word[idx] == secret_word[idx]:
         emoji_str += GREEN_BOX
         idx += 1
-    # If match not present:
+    # If no match at index:
     else:
         chr_present: bool = False  # Variable for a character's presence, initially False
         altidx: int = 0  # Initializes variable to hold alternative indexes of the secret word
         
-# Loop runs while character is not present and the other index is below the secret length
+# Loop runs while character is not present and the other index is less than the secret length
         while not chr_present and altidx < secret_len:
             if secret_word[altidx] == user_word[idx]:
                 chr_present = True  
