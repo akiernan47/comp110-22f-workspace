d1ddf853ad2ec2689bd49b8e2b4d472831f63ab8
Aidan Kiernan <akiernan@unc.edu>
2022-08-30 22:39:53 -0400

    Finale

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -48,7 +48,7 @@ while idx < secret_len:
             idx += 1  # Increase count for the scope of the outer loop
         
 print(emoji_str)  
-# Complete emoji string is output -> # of boxes = # of characters in the secret word
+# Complete emoji string is output at the length of the secret word
 
 # Final guess checking conditional:
 if len(user_word) == secret_len:
