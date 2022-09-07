6765272f1dcf578a2944893896d877f0bcfaa2ff
Aidan Kiernan <akiernan@unc.edu>
2022-09-07 00:14:00 -0400

    new!

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -2,38 +2,40 @@
 
 __author__ = "730515426"
 
-"""Will search for the presence of a character (search_chr), within a given string of any length (searched_word)."""
+
 def contains_char(searched_word: str, search_chr: str) -> bool:
+    """Will search for the presence of a character (search_chr), within a given string of any length (searched_word)."""
     assert len(search_chr) == 1
-    idx: int = 0 # Initialize index count to 0
+    idx: int = 0  # Initialize index count to 0
    
     while idx < len(searched_word):
         if searched_word[idx] == search_chr:
-            return True # Character is present
+            return True  # Character is present
         else:
             idx += 1 
-    return False # The character is not present within the word
+    return False  # The character is not present within the word
     
+
 # Named constants for emoji boxes
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-"""Will return a string of emojis based on the accuracy of a user's guess of the secret word"""
+
 def emojified(secret_guess: str, secret: str) -> str:
+    """Will return a string of emojis based on the accuracy of a user's guess of the secret word."""
     assert len(secret_guess) == len(secret)
-    idx: int = 0 # Initialize index count to 0
-    emoji_str: str = "" # Initialize empty string for eventual emoji output
+    idx: int = 0  # Initialize index count to 0
+    emoji_str: str = ""  # Initialize empty string for eventual emoji output
     while idx < len(secret):
         if secret_guess[idx] == secret[idx]:
-                emoji_str += GREEN_BOX # Matching character at same index
-                idx += 1
-        elif contains_char(secret, secret_guess[idx]) == False:
-            emoji_str += WHITE_BOX # Non-present character
+            emoji_str += GREEN_BOX  # Matching character at same index
+            idx += 1
+        elif contains_char(secret, secret_guess[idx]):
+            emoji_str += YELLOW_BOX  # Character present at differing index
             idx += 1
         else:
-            emoji_str += YELLOW_BOX # Character present at differing index
+            emoji_str += WHITE_BOX  # Non-present character
             idx += 1
 
-
-    return emoji_str # Completed string comprised of corresponding emojis
+    return emoji_str  # Completed string comprised of corresponding emojis
