8da65e3c82363724fd53075d268f54261e444de9
Aidan Kiernan <akiernan@unc.edu>
2022-09-08 13:42:45 -0400

    yup

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -7,6 +7,7 @@ def contains_char(searched_word: str, search_chr: str) -> bool:
     """Will search for the presence of a character within a given string of any length."""
     assert len(search_chr) == 1
     idx: int = 0  # Initialize index count to 0
+    
     while idx < len(searched_word):
         if searched_word[idx] == search_chr:
             return True  # Character is present
@@ -26,6 +27,7 @@ def emojified(secret_guess: str, secret: str) -> str:
     assert len(secret_guess) == len(secret)
     idx: int = 0  # Initialize index count to 0
     emoji_str: str = ""  # Initialize empty string for eventual emoji output
+    
     while idx < len(secret):
         if secret_guess[idx] == secret[idx]:
             emoji_str += GREEN_BOX  # Matching character at same index
@@ -39,11 +41,25 @@ def emojified(secret_guess: str, secret: str) -> str:
     return emoji_str  # Completed string comprised of corresponding emojis
 
 
-def input_guess(guess_len: int) -> int:
+def input_guess(guess_len: int) -> str:
     """Will return a string (of an expected length) given by the user."""
     # Initial user prompt for a guess:
     secret_guess: str = input(f"Enter a {guess_len} character word: ")
+    
     # Runs until the input is of an expected length:
     while len(secret_guess) != guess_len: 
         secret_guess = input(f"That wasn't {guess_len} chars! Try again: ")
     return secret_guess  # String with expected length is returned
+
+
+def main() -> None:
+    """The entrypoint of the program and main game loop."""
+    turns: int = 1  # Turn count initilaized at 1
+    secret: str = "codes"  # Secret user is trying to guess
+    guess: str = "" # Initialize empty string to hold user guess
+    
+    while turns < 7 and secret != guess:
+        print(f"=== Turn {turns}/6 ===")
+        guess = input_guess(len(secret))
+        print(guess)
+        turns += 1
