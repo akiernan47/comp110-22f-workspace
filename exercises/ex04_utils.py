d7530d2e7fe8346fde483c98c9b6bd6e1e43f5e3
Aidan Kiernan <akiernan@unc.edu>
2022-09-16 17:43:56 -0400

    et

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -3,16 +3,22 @@
 __author__ = "730515426"
 
 
-def all(int_list: list[int], given_int: int) -> bool:
+def all(list_input: list[int], given_int: int) -> bool:
     """Will search list of ints and determine whether they all match a given int."""
     i: int = 0
-    nums: list[int] = list(int_list)
-    if len(nums) == 0:
+    if len(list_input) == 0:
         return False
-    while i < len(nums):
-        if given_int == nums[i]:
+    while i < len(list_input):
+        if given_int == list_input[i]:
             i += 1
         else:
             return False
     return True
+
+
+def max(list_input: list[int]) -> bool:
+    """returns the largest item in a given list."""
+    if len(list_input) == 0:
+        raise ValueError("max() arg is an empty List")
+    i: int = 0
 
