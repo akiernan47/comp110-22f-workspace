fd211886114ca4f28c2b15c09f9f7181ecdef4cf
Aidan Kiernan <akiernan@unc.edu>
2022-09-23 18:03:57 -0400

    2/3 done

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -2,19 +2,31 @@
 
 __author__ = "730515426"
 
+
 def only_evens(given_list: list[int]) -> list[int]:
-    """Returns new list only containing even elements"""
+    """Returns new list only containing even elements."""
     i: int = 0
-    evens_list: list[int] = []  # Initialize empty list
-    # If the list is empty:
+    evens_list: list[int] = [] 
     if len(given_list) == 0:
         return evens_list
-    
-    # Loop will check each index for an even character
     while i < len(given_list):
         if given_list[i] % 2 == 0:
-            evens_list.append(given_list[i])  # Appends even int to new list
+            evens_list.append(given_list[i])
             i += 1
         else:
             i += 1
-    return evens_list  # Returns list of even ints found in the given list
+    return evens_list
+
+
+def concat(ints_one: list[int], ints_two: list[int]) -> list[int]:
+    """Returns new list containing all elements of the two lists."""
+    concat_list: list(int) = []
+    one_i: int = 0
+    two_i: int = 0
+    while one_i < len(ints_one):
+        concat_list.append(ints_one[one_i])
+        one_i += 1
+    while two_i < len(ints_two):
+        concat_list.append(ints_two[two_i])
+        two_i += 1
+    return concat_list
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -2,12 +2,45 @@
 
 __author__ = "730515426"
 
+
 from utils import only_evens
+from utils import concat
+
 
 def test_only_evens_empty() -> None:
-    """Test for empty list"""
+    """Test for empty list."""
     given_list: list(int) = []
     assert only_evens(given_list) == []
 
 
-def test_only_evens_odds() -> None
+def test_only_evens_mix1() -> None:
+    """Test for list of mixed ints (odds and evens)."""
+    given_list: list(int) = [1, 2, 3, 4]
+    assert only_evens(given_list) == [2, 4]
+
+
+def test_only_evens_mix2() -> None:
+    """Test for another list of mixed ints (odds and evens)."""
+    given_list: list(int) = [33, 55, 44, 66]
+    assert only_evens(given_list) == [44, 66]
+
+
+def test_concat_both_empty() -> None:
+    """Test for both lists being empty."""
+    ints_one: list(int) = []
+    ints_two: list(int) = []
+    assert concat(ints_one, ints_two) == []
+
+
+def test_concat_normal1() -> None:
+    """Test for expected list use cases of same lengths."""
+    ints_one: list(int) = [1, 2, 3]
+    ints_two: list(int) = [4, 5, 6]
+    assert concat(ints_one, ints_two) == [1, 2, 3, 4, 5, 6]
+
+
+def test_concat_normal2() -> None:
+    """Test for expected list use cases of differing lengths."""
+    ints_one: list(int) = [1, 3, 5]
+    ints_two: list(int) = [3, 1]
+    assert concat(ints_one, ints_two) == [1, 3, 5, 3, 1]
