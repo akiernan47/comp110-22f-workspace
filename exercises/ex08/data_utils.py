cb372177d5fb24f5154243e7cf76c98aacf6b41c
Aidan Kiernan <akiernan@unc.edu>
2022-10-10 17:07:32 -0400

    data

--- exercises/ex08/data_utils.py
+++ exercises/ex08/data_utils.py
@@ -1,5 +1,10 @@
 """Dictionary related utility functions."""
 
-__author__ = ""
+__author__ = "730515426"
 
 # Define your functions below
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read the rows of a csv into a 'table."""
+    result: list[dict[str, str]] = []
+    # TODO: More work?
+    return result
