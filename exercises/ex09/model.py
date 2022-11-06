c5810ad7a6398d18f92cfdefe94d520868643b76
Aidan Kiernan <akiernan@unc.edu>
2022-11-06 01:27:04 -0400

    progress

--- exercises/ex09/model.py
+++ exercises/ex09/model.py
@@ -6,7 +6,7 @@ from exercises.ex09 import constants
 from math import sin, cos, pi
 
 
-__author__ = ""  # TODO
+__author__ = "730515426"
 
 
 class Point:
@@ -30,24 +30,44 @@ class Cell:
     """An individual subject in the simulation."""
     location: Point
     direction: Point
-    sickness: int = 0
+    sickness: int = constants.VULNERABLE
 
-    def __init__(self, location: Point, direction: Point):
+    def __init__(self, location: Point, direction: Point) -> None:
         """Construct a cell with its location and direction."""
         self.location = location
         self.direction = direction
 
-    # Part 1) Define a method named `tick` with no parameters.
-    # Its purpose is to reassign the object's location attribute
-    # the result of adding the self object's location with its
-    # direction. Hint: Look at the add method.
+
     def tick(self) -> None:
+        """New location after tick based on direction."""
         self.location = self.location.add(self.direction)
+
+    
+    def contract_disease(self) -> None:
+        """Cell has become infected."""
+        self.sickness = constants.INFECTED
+
+
+    def is_vulnerable(self) -> bool:
+        """Cell is vulnerable."""
+        if self.sickness == constants.VULNERABLE:
+            return True
+        return False
+
+
+    def is_infected(self) -> bool:
+        """Cell is infected."""
+        if self.sickness == constants.INFECTED:
+            return True
+        return False
 
 
     def color(self) -> str:
         """Return the color representation of a cell."""
-        return "black"
+        if self.is_infected() == True:
+            return "red"
+        else:
+            return "gray"
 
 
 class Model:
@@ -56,14 +76,23 @@ class Model:
     population: list[Cell]
     time: int = 0
 
-    def __init__(self, cells: int, speed: float):
+    def __init__(self, cells: int, speed: float, infected: int) -> None:
         """Initialize the cells with random locations and directions."""
         self.population = []
-        for _ in range(cells):
+        if infected >= cells or infected <= 0:
+            raise ValueError("Improper starting infected value")
+        for _ in range(infected, cells):
             start_location: Point = self.random_location()
             start_direction: Point = self.random_direction(speed)
             cell: Cell = Cell(start_location, start_direction)
             self.population.append(cell)
+        for _ in range(infected):
+            start_location: Point = self.random_location()
+            start_direction: Point = self.random_direction(speed)
+            cell: Cell = Cell(start_location, start_direction)
+            cell.contract_disease()
+            self.population.append(cell)
+        
     
     def tick(self) -> None:
         """Update the state of the simulation by one time step."""
@@ -72,11 +101,13 @@ class Model:
             cell.tick()
             self.enforce_bounds(cell)
 
+
     def random_location(self) -> Point:
         """Generate a random location."""
         start_x: float = random() * constants.BOUNDS_WIDTH - constants.MAX_X
         start_y: float = random() * constants.BOUNDS_HEIGHT - constants.MAX_Y
         return Point(start_x, start_y)
+
 
     def random_direction(self, speed: float) -> Point:
         """Generate a 'point' used as a directional vector."""
@@ -84,6 +115,7 @@ class Model:
         direction_x: float = cos(random_angle) * speed
         direction_y: float = sin(random_angle) * speed
         return Point(direction_x, direction_y)
+
 
     def enforce_bounds(self, cell: Cell) -> None:
         """Cause a cell to 'bounce' if it goes out of bounds."""
@@ -99,6 +131,7 @@ class Model:
         elif cell.location.y < constants.MIN_Y:
             cell.location.y = constants.MIN_Y
             cell.direction.y *= -1.0
+
 
     def is_complete(self) -> bool:
         """Method to indicate when the simulation is complete."""
